9785209c16ad9a57a17e945ad06901a0c308a024
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-05-05 09:12:11 -0500

    Create Ejercicio3.cpp

---
+++ PracticaN°1/Ejercicio3.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+ 
+using namespace std;
+
+void cifrar(string texto){
+    
+    for (int i=0;i<texto.length();i++){
+        if(texto[i]>='a' && texto[i]<='z' || texto[i]>='A' && texto[i]<='Z' ){
+            texto[i]+=3;
+            cout<<texto[i];
+        }
+        else
+            cout<<texto[i];
+    }
+    cout<<endl;
+}
+
+void descifrar(string texto){
+    for (int i=0;i<texto.length();i++){
+        if(texto[i]>='a' && texto[i]<='z' || texto[i]>='A' && texto[i]<='Z' ){
+            texto[i]-=3;
+            cout<<texto[i];
+        }
+        else
+            cout<<texto[i];
+    }
+    cout<<endl;
+}
+
+int main() {
+    ifstream archivo;
+    string texto;
+    int opc;
+    cout<<":::::::::::CIFRADO DE CESAR:::::::::::"<<endl;
+    cout<<endl;
+    cout<<"Elija una opción :"<<endl;
+    cout<<"1.Cifrar pseudocodigo"<<endl;
+    cout<<"2.Descifrar pseudocodigo"<<endl;
+    cin>>opc;
+    cout<<endl;
+    archivo.open("compilar.txt",ios::in);
+    switch (opc)
+    {
+        case 1:
+            cout<<":::::::::::Pseudocodigo cifrado:::::::::::"<<endl;
+            cout<<endl;
+            while(!archivo.eof()){//mientras no sea el final
+                getline(archivo,texto);
+                cifrar(texto);
+            }
+            archivo.close();
+            break;
+        
+        default:
+            cout<<":::::::::::Pseudocodigo descifrado:::::::::::"<<endl;
+            cout<<endl;
+            while(!archivo.eof()){//mientras no sea el final
+                getline(archivo,texto);
+                descifrar(texto);
+            }
+            archivo.close();
+            break;
+    }
+    return 0;
+}
+/*
+Pseudocódigo de un programa que permita leer la edad
+y peso de una persona y posteriormente imprimirla.
+
+:::::::::::::::SIN CIFRAR::::::::::::::::
+
+Inicio
+
+Variables edad, peso.
+
+Mostrar "Escribir los datos (Edad, Peso):"
+
+Leer Edad, Leer Peso.
+
+Mostrar "Tu peso es: ", peso, "Tu edad es: ", edad.
+
+Fin.
+
+::::::::::::::::CIFRADO:::::::::::::::::::::
+
+Lqlflr
+
+Yduldeohv hgdg, shvr.
+
+Prvwudu "Hvfulelu orv gdwrv (Hgdg, Shvr):"
+
+Ohhu Hgdg, Ohhu Shvr.
+
+Prvwudu "Wx shvr hv: ", shvr, "Wx hgdg hv: ", hgdg.
+
+Ilq.
+*/
