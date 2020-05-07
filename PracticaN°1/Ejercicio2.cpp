1eda07e387b5278041e87dbbd03b2a13da8917f7
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-05-07 12:20:21 -0500

    Update Ejercicio2.cpp

--- PracticaN°1/Ejercicio2.cpp
+++ PracticaN°1/Ejercicio2.cpp
@@ -1,4 +1,3 @@
-
 #include<fstream>
 #include <string>
 #include <iostream>
@@ -10,20 +9,23 @@ int main() {
     archivo.open("compilar.txt",ios::in);
     while(!archivo.eof()){//mientras no sea el final
         getline(archivo,texto);
-        for (int i=0;i<texto.length();i++)
-            if(texto[i]!=' ')
+        for (int i = 0; i < texto.length(); i++)
+            if(texto[i] != ' ')
                 cout<<texto[i]<<endl;
     }
     archivo.close();
     return 0;
 }
 
-/* compilar.txt
+/* 
+TEXTO PLANO
+:::::::::::::::compilar.txt:::::::::::::::::::
+
 Inicio
-Mostrar "Introduzca el diametro, en metros": Pedir D
-Mostrar "Introduzca la altura, en metros": Pedir H
-R = D/2: Pi = 3,141593
-V = Pi * (R ^ 2) * H
-Mostrar "El volumen del cilindro es de", V, "metros cubicos"
-Fin
+Variables edad, peso.
+Mostrar "Escribir los datos (Edad, Peso):"
+Leer Edad, Leer Peso.
+Mostrar "Tu peso es: ", peso, ",tu edad es: ", edad.
+Fin.
+
 */
