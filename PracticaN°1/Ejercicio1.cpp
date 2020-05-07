f91b9416eecb55069fa6fa610d5be728a5e54a8e
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-05-07 12:19:47 -0500

    Update Ejercicio1.cpp

--- PracticaN°1/Ejercicio1.cpp
+++ PracticaN°1/Ejercicio1.cpp
@@ -5,20 +5,16 @@ using namespace std;
 int main() {
     vector<char> pila;
     char c;
-    // in
     while (cin >> c) {
         pila.push_back(c);
         if (pila[pila.size()-1] == ';') {
             break;
         }   
     }
-    // out
-
-    for (int i=0;i<pila.size();i++) {
-        if(pila[i]!=' ')
-            cout<<pila[i]<<endl;
+    for (int i = 0; i < pila.size(); i++) {
+        if(pila[i] != ' '){
+            cout << pila[i]<<endl;
+        }
     }
-    cout << endl;
-
     return 0;
 }
