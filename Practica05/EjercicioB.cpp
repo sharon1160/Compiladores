aafa1a365467e281ebb3d6a5805a9d79e83f6ec2
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-06-13 16:38:35 -0500

    Create EjercicioB.cpp

---
+++ Practica05/EjercicioB.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+#include <cmath>
+#include <cstdlib> 
+#include <string>
+
+using namespace std;
+
+double evalPosFija( )
+{
+    stack<double> pila;
+    string cad;
+    double a, b, result;
+    cout<<endl;
+    cout<<"cadena ?"<<endl;
+    cin>> cad;
+    while (cad[0] != '=')
+    {
+        result = atof(cad.c_str());
+        if (result != 0.0 ){
+            pila.push(result);
+        }
+        else if (cad == "0.0"){
+            pila.push(result);
+        }
+        else{
+             switch (cad[0])
+            {
+                case '+' : 
+                    a = pila.top(); 
+                    pila.pop(); 
+                    b = pila.top();
+                    pila.pop(); 
+                    pila.push(a+b); 
+                    break;
+                case '-' : 
+                    a = pila.top(); 
+                    pila.pop(); 
+                    b = pila.top();
+                    pila.pop(); 
+                    pila.push(a-b); 
+                    break;
+            }
+        }
+        cout<<"cadena?"<<endl;
+        cin>> cad;
+    }
+    return pila.top();
+}
+
+int main()
+{
+
+    cout<<"Resulado final : "<<evalPosFija()<<endl;
+    return 0;
+}
