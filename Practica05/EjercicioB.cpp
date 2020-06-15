d2420fa03860446d98a877b787c1a8781035a4e9
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-06-15 17:22:08 -0500

    Update EjercicioB.cpp

--- Practica05/EjercicioB.cpp
+++ Practica05/EjercicioB.cpp
@@ -40,6 +40,27 @@ double evalPosFija( )
                     pila.pop(); 
                     pila.push(a-b); 
                     break;
+                case '*' : 
+                    a = pila.top(); 
+                    pila.pop(); 
+                    b = pila.top();
+                    pila.pop(); 
+                    pila.push(a*b); 
+                    break;
+                case '/' : 
+                    a = pila.top(); 
+                    pila.pop(); 
+                    b = pila.top();
+                    pila.pop(); 
+                    pila.push(b/a); 
+                    break;
+                case '^' : 
+                    a = pila.top(); 
+                    pila.pop(); 
+                    b = pila.top();
+                    pila.pop(); 
+                    pila.push(exp(a*log(b)));
+                    break;
             }
         }
         cout<<"cadena?"<<endl;
@@ -48,9 +69,11 @@ double evalPosFija( )
     return pila.top();
 }
 
+
 int main()
 {
-
+    //si la expresion no es Posfija mostrara
+    //Segmentation fault
     cout<<"Resulado final : "<<evalPosFija()<<endl;
     return 0;
 }
